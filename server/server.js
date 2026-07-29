a21d3737f1513e0fefd73964300b2467b51562cc
aeshaShawrma <ui24cs05@iiitsurat.ac.in>
2026-07-29 09:38:05 +0530

    initial express backend setup

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json())
+const PORT = 3000;
+
+app.post("/register", (req, res) => {
+
+    console.log(req.body);
+
+    res.json({
+        message: "User registered successfully"
+    });
+
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
