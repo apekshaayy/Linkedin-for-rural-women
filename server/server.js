da1e25b5f1f7f2244d775a202f34214c55e259a9
aeshaShawrma <ui24cs05@iiitsurat.ac.in>
2026-07-31 00:38:23 +0530

    Add MongoDB user registration

--- server/server.js
+++ server/server.js
@@ -1,17 +1,53 @@
+const User = require("./models/User");
 const express = require("express");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
 
 const app = express();
 app.use(express.json())
 const PORT = 3000;
 
-app.post("/register", (req, res) => {
-
-    console.log(req.body);
-
-    res.json({
-        message: "User registered successfully"
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("MongoDB connected");
+    })
+    .catch((error) => {
+        console.log("MongoDB connection error:", error);
     });
+// Register Route
+app.post("/register", async (req, res) => {
+    console.log("REGISTER ROUTE HIT");
+    console.log("Received data:", req.body);
 
+    try {
+        const { name, email, password } = req.body;
+
+        const newUser = new User({
+            name: name,
+            email: email,
+            password: password
+        });
+
+        console.log("User object created:", newUser);
+
+        const savedUser = await newUser.save();
+
+        console.log("USER SAVED:", savedUser);
+
+        res.status(201).json({
+            success: true,
+            message: "User registered successfully"
+        });
+
+    } catch (error) {
+        console.log("ERROR SAVING USER:", error);
+
+        res.status(500).json({
+            success: false,
+            message: "Registration failed"
+        });
+    }
 });
 
 app.listen(PORT, () => {
